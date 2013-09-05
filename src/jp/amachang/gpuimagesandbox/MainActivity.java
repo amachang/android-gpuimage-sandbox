21721f5eb202f39a43361545b4906ae52704b4db
Hitoshi Amano <seijro@gmail.com>
2013-09-05 18:01:04 +0900

    add sample code

--- src/jp/amachang/gpuimagesandbox/MainActivity.java
+++ src/jp/amachang/gpuimagesandbox/MainActivity.java
@@ -1,22 +1,54 @@
 package jp.amachang.gpuimagesandbox;
 
-import android.os.Bundle;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import jp.co.cyberagent.android.gpuimage.GPUImage;
+import jp.co.cyberagent.android.gpuimage.GPUImageSepiaFilter;
 import android.app.Activity;
-import android.view.Menu;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    private static final String TAG = "GPUIMAGE_SANDBOX";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onClick(View v) {
+        GPUImage image = new GPUImage(this);
+        Bitmap srcBitmap = BitmapFactory.decodeResource(getResources(), R.raw.sample); // LARGE IMAGE!
+        image.setImage(srcBitmap);
+        image.setFilter(new GPUImageSepiaFilter());
+        
+        Log.d(TAG, "Start filter: " + System.currentTimeMillis());
+        
+        Bitmap dstBitmap = image.getBitmapWithFilterApplied(); // HEAVY!
+        
+        Log.d(TAG, "End filter: " + System.currentTimeMillis());
+        
+        String filePath = Environment.getExternalStorageDirectory().getPath() + "/filtered.png";
+        try {
+            FileOutputStream out = new FileOutputStream(filePath);
+            dstBitmap.compress(CompressFormat.PNG, 80, out);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
+        Log.d(TAG, "Saved: filePath = " + filePath);
     }
 
 }
